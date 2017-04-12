649252006254d2f3c470a738fb6fdd9ccd82005a
bogdan <tokarskii.b@gmail.com>
2017-04-12 22:41:18 +0300

    header+content styles add

--- wp-content/themes/startsmart-theme/header.php
+++ wp-content/themes/startsmart-theme/header.php
@@ -21,29 +21,30 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'start-smart' ); ?></a>
-
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
-
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'start-smart' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
+        <div class="row">
+            <div class="container">
+                <nav id="site-navigation" class="main-navigation" role="navigation">
+                    <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'start-smart' ); ?></button>
+                    <?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
+                </nav><!-- #site-navigation -->
+            </div>
+        </div>
+        <?php if ( is_front_page() ) : ?>
+            <div class="landing-header">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12 col-md-6">
+                            <img src="<?php echo get_theme_mod('landing-logo');?>" alt="landing-logo">
+                        </div>
+                        <div class="col-xs-12 col-md-6">
+                            <h1 class="landing-title"><?php echo get_theme_mod('landing-title');?></h1>
+                            <h4 class="landing-sub-title"><?php echo get_theme_mod('landing-sub-title');?></h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php else : ?>
+        <?php endif; ?>
 	</header><!-- #masthead -->
-
 	<div id="content" class="site-content">
