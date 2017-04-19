1db31896d5ffeb70a453c225ca27ed318e1a93e8
Romchigan <ronaldo.mail97@gmail.com>
2017-04-19 17:42:45 +0300

    fixed recent post in single page

--- wp-content/themes/startsmart-theme/single.php
+++ wp-content/themes/startsmart-theme/single.php
@@ -19,16 +19,27 @@
             <h2 class="recent-posts-title recent-posts-blog-page-title text-center section-heading"><?php echo get_theme_mod('recent-posts-blog-page-title');?></h2>
             <p class="recent-posts-description text-center section-description"><?php echo get_theme_mod('recent-posts-description');?></p>
             <div class="row">
-                <?php query_posts('post_type=post&posts_per_page=3'); ?>
-                <?php if(have_posts()) : while (have_posts() ) : the_post(); ?>
-                    <div class="col-xs-12 col-md-4 recent-post section-description">
-												<a href="<?php the_permalink(); ?>"><div class="recent-post-img"><?php the_post_thumbnail(); ?></div></a>
-                        <?php the_category(); ?>
-                        <a href="<?php the_permalink();?>" class="recent-post-title"><h3 class="section-sub-heading"><?php the_title(); ?></h3></a>
-                        <p class="recent-post-description"><?php the_excerpt(); ?></p>
-                    </div>
-                <?php endwhile; endif; ?>
-                <?php wp_reset_query(); ?>
+                <?php
+                    $args=array(
+                        'post__not_in' => array($post->ID),
+                        'showposts'=>3,
+                        'caller_get_posts'=>1);
+                    $my_query = new wp_query($args);
+                    if( $my_query->have_posts() ) {
+                        while ($my_query->have_posts()) {
+                            $my_query->the_post();
+                            ?>
+													<div class="col-xs-12 col-md-4 recent-post section-description">
+														<a href="<?php the_permalink(); ?>"><div class="recent-post-img"><?php the_post_thumbnail(); ?></div></a>
+                              <?php the_category(); ?>
+														<a href="<?php the_permalink();?>" class="recent-post-title"><h3 class="section-sub-heading"><?php the_title(); ?></h3></a>
+														<p class="recent-post-description"><?php the_excerpt(); ?></p>
+													</div>
+                            <?php
+                        }
+                    }
+                    wp_reset_query();
+                ?>
             </div>
         </div>
     </section>
